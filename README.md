e40e86f5486d98e3359a2201931bc63a083afed5
Lanfig <orlandoaf@gmail.com>
2020-09-28 15:11:53 -0300

    Server funcionando como o exemplo do livro

---
+++ README.md
@@ -0,0 +1,14 @@
+# Como usar
+
+* Servidor
+
+`./server 8500 grep foo`
+
+Trocar a porta à vontade, trocar o comando `grep foo` à vontade
+
+* Cliente
+
+`./client 127.0.0.1 8500 < foobar.txt`
+
+Trocar servidor, porta e serviço de acordo com o servidor.
+
--- server.c
+++ server.c
@@ -7,6 +7,92 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <sys/wait.h>
+
+char* inputs[256]; // global; uso específico neste exemplo (universal server)
+
+int install_tcp_server_socket(struct sockaddr_in* addr) {
+    int s = socket(PF_INET, SOCK_STREAM, 0);
+
+    int res_bind = bind(s,(struct sockaddr*) addr, sizeof(struct sockaddr_in));
+    if (res_bind < 0) {
+        perror("bind");
+        exit(2);
+    }
+    int res_listen = listen(s, 10);
+    if (res_listen <0) {
+	perror("listen");
+	exit(2);
+    }
+    return s;
+}
+
+void treat_connection(int server, int client);
+
+// treat_connection é uma global, pois C não tem funções de ordem superior
+void tcp_server(struct sockaddr_in* addr) {
+    signal(SIGPIPE, SIG_IGN);
+    int server_sock = install_tcp_server_socket(addr);
+    int client;
+
+    socklen_t addr_size = sizeof(struct sockaddr_in);
+    
+    while(1) {
+        client = accept(server_sock, (struct sockaddr*) addr, &addr_size);
+	if (client < 0) {
+	    perror("accept");
+	    exit(2);
+	}
+	treat_connection(server_sock, client);
+    }
+}    
+
+void service(int client_sock);
+
+void fork_treatment(int server, int client) {
+    
+    if (fork() == 0) {
+
+        close(server);
+	service(client);
+	exit(0);
+
+    }
+
+    close(client); // desative esta linha e observe o efeito com `ps aux | grep client`
+}
+
+void double_fork_treatment(int server, int client) {
+
+    int k, status;
+
+    k = fork();
+
+    if (k == 0) {
+
+	if (fork() != 0) {
+            // processo filho do original não tem função, e apenas termina 
+            exit(0); 
+	} else {
+	    // processo neto do original executa o serviço, e termina
+	    close(server);
+	    service(client);
+	    exit(0);
+	}
+
+    } else {
+	// processo original espera o filho terminar e retorna
+	waitpid(k, &status, 0);
+
+    }
+
+    close(client);
+}
+
+void treat_connection(int server, int client) {
+    // PENDENTE: registro das conexões (logging)
+    fork_treatment(server, client);
+}
 
 int
 main(int argc, char** argv) {
@@ -18,7 +104,11 @@ main(int argc, char** argv) {
 
     int port = atoi(argv[1]);
 
-    char* args[3] = {"ls", "-l", NULL};
+    int i;
+    for (i = 0; i < argc - 2; i++)
+        inputs[i] = argv[i + 2];
+    inputs[i] = NULL;    
+
 
     struct in_addr host;
     inet_aton("127.0.0.1", &host);
@@ -28,39 +118,17 @@ main(int argc, char** argv) {
     addr.sin_addr = host;
     addr.sin_port = htons(port);
 
-    //==========================================================================
-    //install_tcp_server_socket
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
-
-    int res_bind = bind(sock,(struct sockaddr*) &addr, sizeof(struct sockaddr_in));
-    if (res_bind < 0) {
-        perror("bind");
-        exit(2);
-    }
-    int res_listen = listen(sock, 10);
-    if (res_listen <0) {
-	perror("listen");
-	exit(2);
-    }
-    //fim de install_tcp_server_socket com resultado em sock
-    
-    //==========================================================================
-    //tcp_server
-    signal(SIGPIPE, SIG_IGN);
-    int server_sock = sock;
-    int client;
-
-    while(1) {
-        client = accept(server_socket, (struct sockaddr*) &addr, sizeof(struct sockaddr_in)i);
-	if (client < 0) {
-	    perror("accept");
-	    exit(2);
-	}
-
-	//---------------------------------------------------------------------
-	
-
-    }
+    tcp_server(&addr);
 
 }
+
+void service(int c) {
+    dup2(c, 0);
+    dup2(c, 1);
+    dup2(c, 2);
+    close(c);
+
+    execvp(inputs[0], inputs);
+}
+
 
